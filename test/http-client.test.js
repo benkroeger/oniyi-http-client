1c8c7dc57b1ce1bc80a16f1a32cac813f235bf59
Benjamin Kröger <benjamin.kroeger@gis-ag.com>
2017-12-28 18:32:42 +0100

    unit tests for http-client

--- test/http-client.test.js
+++ test/http-client.test.js
@@ -7,52 +7,37 @@ import _ from 'lodash';
 // internal modules
 import httpClientFactory from '../lib/http-client';
 
-test('client methods', (t) => {
-  const client = httpClientFactory({});
-
+(() => {
+  const client = httpClientFactory();
   ['use', 'makeRequest', 'get', 'put', 'post', 'del', 'head', 'options'].forEach(methodName =>
-    t.true(_.isFunction(client[methodName]), `client should have "${methodName}" method`));
+    test(`client instance exposes method '${methodName}'`, t =>
+      t.true(_.isFunction(client[methodName]), `client should have "${methodName}" method`)));
+})();
+
+(() => {
+  const client = httpClientFactory();
+  ['makeRequest', 'get', 'put', 'post', 'del', 'head', 'options'].forEach(methodName =>
+    test(`'${methodName}' method returns a Promise `, (t) => {
+      const returnValue = client[methodName]();
+      const resolved = Promise.resolve(returnValue);
+      t.is(returnValue, resolved);
+    }));
+})();
+
+test("'use' method returns client instance", (t) => {
+  const client = httpClientFactory();
+  const returnValue = client.use({ name: 'noop' });
+
+  t.is(returnValue, client);
 });
 
-test.cb('simple request', (t) => {
-  const client = httpClientFactory({});
 
-  client.get('http://httpstat.us/200', (err, response, body) => {
-    t.ifError(err);
-    t.log(body);
-    t.end();
-  });
-});
-
-test.cb('with plugin', (t) => {
-  const client = httpClientFactory({});
-
-  const plugin = {
-    name: 'plugin1',
-    onRequest: [
-      {
-        phaseName: 'initial',
-        handler: (ctx, next) => {
-          t.log('in plugin -> onRequest:initial');
-          next();
-        },
-      },
-    ],
-    onResponse: [
-      {
-        phaseName: 'final',
-        handler: (ctx, next) => {
-          t.log('in plugin -> onResponse:final');
-          next();
-        },
-      },
-    ],
-  };
-
-  client.use(plugin);
-  client.get('http://httpstat.us/200', (err, response, body) => {
-    t.ifError(err);
-    t.log(body);
-    t.end();
-  });
-});
+// test.cb('simple request', (t) => {
+//   const client = httpClientFactory({});
+//
+//   client.get('http://httpstat.us/200', (err, response, body) => {
+//     t.ifError(err);
+//     t.log(body);
+//     t.end();
+//   });
+// });
